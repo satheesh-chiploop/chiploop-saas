19b22ca0031ce01589d1969fbd1c84bd74b4f93a
satheesh-chiploop <pillaisatheesh74@gmail.com>
2025-10-15 20:06:21 -0700

    page file update for scroll feature

--- frontend/app/workflow/page.tsx
+++ frontend/app/workflow/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+// 10-15
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
