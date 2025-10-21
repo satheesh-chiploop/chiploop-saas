b2e749124c7a064a29c5e0e41a094a0abef3b646
satheesh-chiploop <pillaisatheesh74@gmail.com>
2025-10-21 10:14:16 -0700

    datatime -> datetime fixes

--- backend/planner/ai_agent_planner.py
+++ backend/planner/ai_agent_planner.py
@@ -3,7 +3,7 @@ from loguru import logger
 from utils.llm_utils import run_llm_fallback
 from utils.supabase_utils import supabase
 from planner.ai_work_planner import analyze_spec
-from datatime import datatime
+from datetime import datatime
 
 
 async def fetch_user_memory(user_id: str):
