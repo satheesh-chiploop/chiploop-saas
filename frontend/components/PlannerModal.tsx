b12bf1e90b76b0366f77e356678726663331851f
satheesh-chiploop <pillaisatheesh74@gmail.com>
2025-10-21 11:32:56 -0700

    crash issue resolved

--- frontend/components/PlannerModal.tsx
+++ frontend/components/PlannerModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useVoiceAnalyzer } from "@/hooks/useVoiceAnalyzer";
 
 export default function PlannerModal({ onClose }) {
