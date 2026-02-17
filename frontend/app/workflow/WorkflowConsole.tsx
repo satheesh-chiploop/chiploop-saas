721a36f812afd8a935b2c3f6ac104ad046e90a1f
satheesh-chiploop <pillaisatheesh74@gmail.com>
2026-02-16 18:55:12 -0800

    Adding files

--- frontend/app/workflow/WorkflowConsole.tsx
+++ frontend/app/workflow/WorkflowConsole.tsx
@@ -8,7 +8,7 @@ import { useRouter } from "next/navigation";
 //const supabase = createClient(
 //  process.env.NEXT_PUBLIC_SUPABASE_URL!,
 //  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+// );
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 const supabase = createClientComponentClient();
